b7e9550401f6fff0b3cad342b36a76ed1b50e465
nabilfaturr <nabilfaturr44@gmail.com>
2024-09-05 15:45:30 +0700

    style: change cta distance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
 const Hero = () => {
   return (
-    <div className="pt-24 flex flex-col gap-9">
+    <div className="pt-20 flex flex-col gap-5">
       <div className="w-32 h-32 rounded-full overflow-hidden">
         <Image
           src={profPic}
@@ -24,26 +24,31 @@ const Hero = () => {
           className="size-full object-cover object-top"
         />
       </div>
-      <div className={`space-y-2`}>
-        <h2 className="text-xl sm:text-2xl font-bold flex flex-col">
-          <span className="flex items-center gap-2">
-            <WavingHand />
-            Hello World!
-          </span>
-          <span className="text-2xl xs:text-3xl sm:text-4xl">
-            You can call me <span className="font-bold bg-gradient-to-r from-teal-200 to-teal-400">Nabil</span>
-          </span>
-        </h2>
-        <p
-          className={`text-foreground/80 max-w-xl text-lg flex flex-col gap-2 ${inter.className}`}
-        >
-          I&apos;m a fullstack developer. love to build end-to-end web application
-          and design system. Whenever I feel like I should share my knowledge
-          with others, I like to do it on my blog.
-          <TimezoneBadge />
-        </p>
+      <div className="flex flex-col gap-5">
+        <div className={`space-y-2`}>
+          <h2 className="text-xl sm:text-2xl font-bold flex flex-col">
+            <span className="flex items-center gap-2">
+              <WavingHand />
+              Hello World!
+            </span>
+            <span className="text-2xl xs:text-3xl sm:text-4xl">
+              You can call me{" "}
+              <span className="font-bold bg-gradient-to-r from-teal-200 to-teal-400">
+                Nabil
+              </span>
+            </span>
+          </h2>
+          <p
+            className={`text-foreground/80 max-w-xl text-lg flex flex-col gap-2 ${inter.className}`}
+          >
+            I&apos;m a fullstack developer. love to build end-to-end web
+            application and design system. Whenever I feel like I should share
+            my knowledge with others, I like to do it on my blog.
+          </p>
+        </div>
+        <TimezoneBadge />
+        <CTA />
       </div>
-      <CTA />
     </div>
   );
 };
@@ -58,7 +63,12 @@ const CTA = () => {
       <Button className="bg-teal-600 text-white hover:bg-teal-600/80 transition-all duration-300">
         Read my blog
       </Button>
-      <Button variant="outline" className="bg-teal-600/10 text-teal-600 border-0 hover:bg-teal-600/20 hover:text-teal-600/80 transition-all duration-300">Explore my work</Button>
+      <Button
+        variant="outline"
+        className="bg-teal-600/10 text-teal-600 border-0 hover:bg-teal-600/20 hover:text-teal-600/80 transition-all duration-300"
+      >
+        Explore my work
+      </Button>
     </div>
   );
 };
