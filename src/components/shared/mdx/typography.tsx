7e942a755fd4e9b58b7663b6ca511dbaf18aa521
nabilfaturr <nabilfaturr44@gmail.com>
2024-09-12 21:41:40 +0700

    feat(blog): add typography

---
+++ src/components/shared/mdx/typography.tsx
@@ -0,0 +1,49 @@
+import { GeistMono } from "geist/font/mono";
+
+export const Text = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <p className="text-black/75 mb-4 dark:text-white/75 leading-9">
+      {children}
+    </p>
+  );
+};
+
+export const Heading = ({
+  children,
+  level,
+}: {
+  children: React.ReactNode;
+  level: 1 | 2 | 3;
+}) => {
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+  const headingClasses = {
+    1: "text-4xl font-bold mb-2 mt-7",
+    2: "text-3xl font-bold mb-2 mt-12",
+    3: "text-2xl font-bold mb-2 mt-12",
+  };
+
+  return <Tag className={headingClasses[level]}>{children}</Tag>;
+};
+
+export const Code = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <code
+      className={`${GeistMono.className} bg-teal-100 border border-teal-200 text-teal-700 px-1 py-0.5 rounded-md dark:bg-teal-900 dark:text-teal-300 dark:border-teal-800 leading-9 font-bold`}
+    >
+      {children}
+    </code>
+  );
+};
+
+export const UnorderedList = ({ children }: { children: React.ReactNode }) => {
+  return <ul className="list-disc mb-4 pl-8">{children}</ul>;
+};
+
+export const OrderedList = ({ children }: { children: React.ReactNode }) => {
+  return <ol className="list-decimal mb-4 pl-8">{children}</ol>;
+};
+
+export const ListItem = ({ children }: { children: React.ReactNode }) => {
+  return <li className="pl-2 pb-3 mb-3">{children}</li>;
+};
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,23 @@
 import type { MDXComponents } from "mdx/types";
+import {
+  Code,
+  Heading,
+  OrderedList,
+  Text,
+  UnorderedList,
+  ListItem,
+} from "./components/shared/mdx/typography";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    p: ({ children }) => <Text>{children}</Text>,
+    h1: ({ children }) => <Heading level={1}>{children}</Heading>,
+    h2: ({ children }) => <Heading level={2}>{children}</Heading>,
+    h3: ({ children }) => <Heading level={3}>{children}</Heading>,
+    code: ({ children }) => <Code>{children}</Code>,
+    ul: ({ children }) => <UnorderedList>{children}</UnorderedList>,
+    ol: ({ children }) => <OrderedList>{children}</OrderedList>,
+    li: ({ children }) => <ListItem>{children}</ListItem>,
     ...components,
   };
 }
