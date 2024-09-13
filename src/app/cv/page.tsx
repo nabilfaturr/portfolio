8bf6b23c32eb2cc3ade1bea1a13f3770e953364b
nabilfaturr <nabilfaturr44@gmail.com>
2024-09-14 01:19:10 +0700

    feat: add cv

---
+++ src/app/cv/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function CVPage() {
+  redirect(
+    "https://docs.google.com/document/u/4/d/1P7eUnOlZZ0nYGklvbJXSCirf9JvTj9sAs--N5ym5M7k/pub"
+  );
+}
