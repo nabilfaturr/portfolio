fb1e69651b8a994017cc5f3fb5715ca55495c4b4
nabilfaturr <nabilfaturr44@gmail.com>
2024-09-14 23:57:36 +0700

    fix: prevent layout rendering before redirect

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function CVPage() {
   const cv_pdf_url = `https://drive.google.com/file/d/15vT6ni8OHpgYqQtKvBBJdlhxE4E9KElk/view?usp=sharing`;
   redirect(cv_pdf_url);
