3002a36d4dfcf70d52c52f264b42faecf4e43bac
nabilfaturr <nabilfaturr44@gmail.com>
2024-09-20 04:24:48 +0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { ThemeProvider } from "@/components/shared/provider/themes-provider";
@@ -28,6 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SpeedInsights />
+          <Analytics />
           <Header />
           <main className="container">{children}</main>
           <Footer />
